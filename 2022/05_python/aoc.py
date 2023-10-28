d9c97b49a43e38a151851d2b41eee1d4f30077f6
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2023-10-28 17:17:10 -0300

    feat: a think that's better python code

--- 2022/05_python/aoc.py
+++ 2022/05_python/aoc.py
@@ -1,42 +1,68 @@
 input = open("./assets/index.txt", "r").readlines()
 
-crates = []
 
-for line in input:
-    if not line.startswith(" 1 "):
-        crates.append(line.strip("\n"))
-    else:
-        break
+def get_crates(input):
+    crates = []
 
-stacks = [[] for _ in range(0, len(input[0]), 4)]
+    for line in input:
+        if not line.startswith(" 1 "):
+            crates.append(line.strip("\n"))
+        else:
+            break
 
-for i, line in enumerate(crates):
-    row = [line[i : i + 4] for i in range(0, len(line), 4)]
-
-    for ii, crate in enumerate(row):
-        char = crate.strip("[] ")
-
-        if char:
-            stacks[ii].append(char)
-
-moves = []
-
-for line in input:
-    if line.startswith("move"):
-        moves.append([int(x) for i, x in enumerate(line.split()) if (i + 1) % 2 == 0])
-
-for move in moves:
-    list = stacks[move[1] - 1][0 : move[0]]
-    list.reverse()  # For part 2.
-
-    for i in list:
-        stacks[move[2] - 1].insert(0, i)
-
-    del stacks[move[1] - 1][0 : move[0]]
+    return crates
 
 
-print("\nResult: ")
-for stack in stacks:
+def get_stacks(input):
+    stacks = [[] for _ in range(0, len(input[0]), 4)]
+
+    for i, line in enumerate(get_crates(input)):
+        row = [line[i : i + 4] for i in range(0, len(line), 4)]
+
+        for ii, crate in enumerate(row):
+            char = crate.strip("[] ")
+
+            if char:
+                stacks[ii].append(char)
+
+    return stacks
+
+
+def get_moves(input):
+    moves = []
+
+    for line in input:
+        if line.startswith("move"):
+            moves.append(
+                [int(x) for i, x in enumerate(line.split()) if (i + 1) % 2 == 0]
+            )
+
+    return moves
+
+
+def solve_moves(input, reverse=False):
+    stacks = get_stacks(input)
+
+    for move in get_moves(input):
+        list = stacks[move[1] - 1][0 : move[0]]
+
+        if reverse:
+            list.reverse()  # For part 2.
+
+        for i in list:
+            stacks[move[2] - 1].insert(0, i)
+
+        del stacks[move[1] - 1][0 : move[0]]
+
+    return stacks
+
+
+print("\nPart 1: ")
+for stack in solve_moves(input):
+    print(stack[0], end="")
+
+print("\n\nPart 2: ")
+for stack in solve_moves(input, reverse=True):
     print(stack[0], end="")
 
 print("\n")
